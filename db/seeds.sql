6bc259dbf29d3747bc0c0bb66cc8c9cc921b6b36
Diana Carrara <dianaVcarrara@gmail.com>
2019-06-23 16:29:37 -0600

    added handlebars pages

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,4 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) VALUES ('777 Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Glamburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wendys Lobster & Caviar Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Douche Burger', false);
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -20,10 +19,6 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(port);
